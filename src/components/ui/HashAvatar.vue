b3cd63aa20218fb28d89f634eb853c55a85cb64b
Stefan Oancea <stefan.oancea93@gmail.com>
2018-10-17 22:45:48 +0300

    Remove old comms

--- src/components/ui/HashAvatar.vue
+++ src/components/ui/HashAvatar.vue
@@ -14,11 +14,11 @@ export default {
   filters: {
     dataurl(h) {
       const opts = {
-        foreground: [53, 124, 255, 255],               // rgba black
-        background: [255, 255, 255, 255],         // rgba white
-        margin: 0.1,                              // 20% margin
-        size: 72,                                // 420px square
-        format: 'svg'                             // use SVG instead of PNG
+        foreground: [53, 124, 255, 255],
+        background: [255, 255, 255, 255],
+        margin: 0.1,
+        size: 72,
+        format: 'svg',
       };
       const icon = new Identicon(h.replace('0x', ''), opts).toString();
       return ('data:image/svg+xml;base64,' + icon);
