57e80e3996d106e353bace48052f346afd1b39ba
Stefan Oancea <stefan.oancea93@gmail.com>
2018-10-18 19:48:12 +0300

    Add tooltip for account hash

--- src/components/ui/AccountHash.vue
+++ src/components/ui/AccountHash.vue
@@ -1,16 +1,15 @@
 <template>
   <div class="wrapper">
+    <div class="top-tooltip-container">
+      <div class="arrow"></div>
+      <div class="top-tooltip">
+        {{value}}
+      </div>
+    </div>
     <div v-if="value != null" class="hash">
       <router-link  :to="{ name: 'account', params: { address: value }}"
                     class="tooltip-target">{{ value | cleanHash }}
       </router-link>
-      <template slot="popover">
-        <p>
-        <strong>
-          {{value}}
-        </strong>
-        </p>
-      </template>
     </div>
   </div>
 </template>
@@ -56,8 +55,40 @@ a {
   line-height: 24px;
   font-weight: 500;
 }
-  .wrapper {
-    padding: 10px 8px;
+
+.wrapper {
+  padding: 10px 8px;
+  position: relative;
+}
+
+.wrapper:hover .top-tooltip-container {
+  display: block;
+}
+
+.top-tooltip-container {
+  display: none;
+  padding: 2px 0 8px;
+  position: absolute;
+  top: -28px;
+  left: calc(-50% - 5px);
+}
+.top-tooltip {
+  background-color: white;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 19px;
+  color: #273656;
+  padding: 4px 16px;
+  box-shadow: 0 2px 6px 0 rgba(0,0,0,0.04);
+}
+  .arrow {
+    position: absolute;
+    bottom: 4px;
+    left: calc(50% - 4px);
+    width: 8px;
+    height: 8px;
+    transform: rotate(45deg);
+    background-color: white;
   }
 
 
