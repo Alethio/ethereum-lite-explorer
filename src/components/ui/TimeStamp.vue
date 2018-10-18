95beac7fb4d41e007db559fb51b1015a6db207dc
Stefan Oancea <stefan.oancea93@gmail.com>
2018-10-18 20:13:45 +0300

    Add Timestamp tooltip

--- src/components/ui/TimeStamp.vue
+++ src/components/ui/TimeStamp.vue
@@ -1,24 +1,15 @@
 <template>
 <div v-if="value != null" class="value">
-
-  <v-popover
-  trigger="hover"
-  placement="top"
-  >
-  <!-- This will be the popover target (for the events and position) -->
+  <div class="top-tooltip-container">
+    <div class="arrow"></div>
+    <div class="top-tooltip">
+      {{value}}
+    </div>
+  </div>
   <div  v-on:click="showTimeAgo = !showTimeAgo" class="tooltip-target cursor" >
     <Timeago v-if="showTimeAgo" :datetime="value * 1000" />
     <span v-if="!showTimeAgo">{{value | timestampToText}}</span>
   </div>
-  <template slot="popover">
-    <p>
-    <strong>
-      {{value}}
-    </strong>
-    </p>
-  </template>
-</v-popover>
-
 </div>
 </template>
 
@@ -50,6 +41,41 @@ export default {
 
 .cursor {
   cursor: pointer;
+}
+.value {
+  position: relative;
+}
+
+.value:hover .top-tooltip-container {
+  display: block;
+}
+
+.top-tooltip-container {
+  display: none;
+  padding: 2px 0 8px;
+  position: absolute;
+  top: -28px;
+  left: 0;
+  right: 0;
+}
+.top-tooltip {
+  text-align: center;
+  background-color: white;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 19px;
+  color: #273656;
+  padding: 4px 16px;
+  box-shadow: 0 2px 6px 0 rgba(0,0,0,0.04);
+}
+.arrow {
+  position: absolute;
+  bottom: 4px;
+  left: calc(50% - 4px);
+  width: 8px;
+  height: 8px;
+  transform: rotate(45deg);
+  background-color: white;
 }
 
 
