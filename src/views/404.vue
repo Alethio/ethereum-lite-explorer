5a4f850e79286b28748f9224f128af578f5d101b
Stefan Oancea <stefan.oancea93@gmail.com>
2018-10-21 16:05:27 +0300

    Add 404 page style

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,12 +1,16 @@
 <template>
-<section class="">
-  <div class="columns">
-    <div class="column has-text-centered">
-    <h1 class="title">Nothing to show</h1>
-    <p>Looks like the resource you are looking for does not exist.</p>
-    <p class="button" v-on:click="lib_goToBlock(lastBlock)">Go to last block</p>
-    </div>
-    <div class="column">
+<section class="home is-fullheight">
+  <div class="error-container">
+    <icon-base icon-name="error"
+               width="48"
+               height="48"
+               viewBox="2 2 20 20"
+    >
+      <icon-error />
+    </icon-base>
+    <div class="error-text">
+      <div>Looks like the resource you are looking for does not exist.</div>
+      <div>Try searching for a different one.</div>
     </div>
   </div>
 </section>
@@ -14,11 +18,14 @@
 
 <script>
 
+import IconBase from '@/components/icons/IconBase.vue';
+import IconError from '@/components/icons/IconError.vue';
 
 export default {
   name: 'NotFounf',
   components: {
-
+    IconBase,
+    IconError,
   },
   data() {
     return ({
