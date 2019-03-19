599c8492534765128cb45e7ad4874323f211e0df
Stefan Oancea <stefan.oancea93@gmail.com>
2019-03-19 13:31:50 +0200

    Fix identicon lint problems, remove dead commented code

--- src/app/typings/identicon.d.ts
+++ src/app/typings/identicon.d.ts
@@ -1,6 +1,5 @@
 declare module "identicon.js" {
     export default class Identicon {
-        constructor(hash: string, options?: Identicon.Options);
         background: number[];
         defaults: {
             background: number[];
@@ -23,12 +22,13 @@ declare module "identicon.js" {
             size: number;
         };
         size: number;
+        constructor(hash: string, options?: Identicon.IOptions);
         image(): any; // PNGlib or Svg
         render(): any;
     }
 
     namespace Identicon {
-        interface Options {
+        interface IOptions {
             size?: number;
             margin?: number;
             foreground?: number[];
@@ -39,32 +39,3 @@ declare module "identicon.js" {
         }
     }
 }
-
-/**
- * Example of identicon object printed with console log:
- *
-Identicon {
-    background: Array(4) [0: 240, 1: 240 2: 240 3: 255] length: 4
-    defaults: {
-        background: Array(4) [0: 240, 1: 240, 2: 240, 3: 255] length: 4
-        brightness: 0.5
-        format: "png"
-        margin: 0.08
-        saturation: 0.7
-        size: 64
-    }
-    foreground: Array(3) [0: 38.25000000000001, 1: 216.75, 2: 86.5753085029323] length: 3
-    format: "png"
-    hash: "ce677205b98d5ea33b4add287c9ef7ba960e26d5"
-    margin: 0.08
-    options: {
-        background: Array(4) [0: 240, 1: 240, 2: 240, 3: 255] length: 4
-        brightness: 0.5
-        format: "png"
-        margin: 0.08
-        saturation: 0.7
-        size: 64
-    }
-    size: 64
-}
-*/
