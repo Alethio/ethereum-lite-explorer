5483ce89b9f836b306c191f7943b4d091255c281
Stefan Oancea <stefan.oancea93@gmail.com>
2019-04-01 15:15:41 +0300

    Update almost all updatable packages

--- src/app/components/toolbar/search/SearchLayer.tsx
+++ src/app/components/toolbar/search/SearchLayer.tsx
@@ -152,7 +152,7 @@ class $SearchLayer extends React.Component<ISearchLayerProps> {
             return;
         }
 
-        let text = e.clipboardData.getData("text/plain");
+        let text = e.clipboardData!.getData("text/plain");
         // Should be non-empty string and it should look like a hash or block number
         if (text && text.match(/^(0x)?[a-fA-F0-9]+$/)) {
             if (!this.props.open) {
