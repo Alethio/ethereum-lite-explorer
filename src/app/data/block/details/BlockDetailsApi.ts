ced90ee3fd4093d49b30659048289edc0a1e00f7
Stefan Oancea <stefan.oancea93@gmail.com>
2019-04-15 15:45:36 +0300

    Add ibft2 support to app (only functional, design tbd)

--- src/app/data/block/details/BlockDetailsApi.ts
+++ src/app/data/block/details/BlockDetailsApi.ts
@@ -1,10 +1,14 @@
 import { BlockDetailsReader } from "./BlockDetailsReader";
 import { Web3EthApi } from "app/data/web3/Web3EthApi";
+import { IBFTReader } from "app/data/block/details/IBFTReader";
+import { AppConfig } from "app/AppConfig";
 
 export class BlockDetailsApi {
     constructor(
+        private appConfig: AppConfig,
         private web3EthApi: Web3EthApi,
-        private blockDetailsReader: BlockDetailsReader
+        private blockDetailsReader: BlockDetailsReader,
+        private ibftReader: IBFTReader
     ) {
 
     }
@@ -14,6 +18,11 @@ export class BlockDetailsApi {
         if (!blockData) {
             throw new Error(`No data found for block #${blockNo}`);
         }
-        return this.blockDetailsReader.read(blockData);
+
+        let bDetails = this.blockDetailsReader.read(blockData);
+        if (this.appConfig.getExtraHeaderType() === "ibft2") {
+            bDetails.ibftExtraData = this.ibftReader.read(blockData.extraData);
+        }
+        return bDetails;
     }
 }
