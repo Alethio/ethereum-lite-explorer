985eb142ec1a44c50276078f0865bcfb6aa9a960
Alexandru Ciuca <alexandru.ciuca@consensys.net>
2019-06-14 10:05:32 +0300

    Set default nodeUrl if env is missing

--- set-env-vars.js
+++ set-env-vars.js
@@ -5,8 +5,10 @@ const fs = require("fs");
 
 let defaultConfig = JSON.parse(fs.readFileSync(path.join(__dirname, "config.default.json"), "utf-8"));
 
-let pluginConfigs = defaultConfig["plugins"];
-let litePluginKey = Object.keys(pluginConfigs).find(k => k.match(/^plugin:\/\/aleth.io\/eth-lite/));
-pluginConfigs[litePluginKey]["nodeUrl"] = process.env.APP_NODE_URL;
+if (process.env.APP_NODE_URL) {
+    let pluginConfigs = defaultConfig["plugins"];
+    let litePluginKey = Object.keys(pluginConfigs).find(k => k.match(/^plugin:\/\/aleth.io\/eth-lite/));
+    pluginConfigs[litePluginKey]["nodeUrl"] = process.env.APP_NODE_URL;
+}
 
 fs.writeFileSync(path.join(__dirname, "config.json"), JSON.stringify(defaultConfig, undefined, "\t"));
