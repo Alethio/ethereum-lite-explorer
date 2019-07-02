ec9a221195925689a57c301931fc959d21a11819
Alexandru Ciuca <alexandru.ciuca@consensys.net>
2019-07-02 14:04:36 +0300

    Escape APP_BASE_URL before passing to sed

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -10,7 +10,8 @@ fi
 
 if [[ ! -z "$APP_BASE_URL" ]]; then
     echo "Setting custom APP_BASE_URL"
-    sed -i.bak "s/https:\/\/lite-explorer.aleth.io/$APP_BASE_URL/" /usr/share/nginx/html/index.html
+    escaped_base_url=$(printf '%s\n' "$APP_BASE_URL" | sed 's:[\/&]:\\&:g;$!s/$/\\/')
+    sed -i.bak "s/https:\/\/lite-explorer.aleth.io/$escaped_base_url/" /usr/share/nginx/html/index.html
 fi
 
 echo "Lite Explorer started"
