792c152280e40b7110f74cbc5dcc021e910af281
Alexandru Ciuca <alexandru.ciuca@consensys.net>
2019-11-28 12:13:14 +0200

    Refactor set-env-vars script

--- set-env-vars.js
+++ set-env-vars.js
@@ -3,7 +3,13 @@
 const path = require("path");
 const fs = require("fs");
 
-let defaultConfig = JSON.parse(fs.readFileSync(path.join(__dirname, "config.default.json"), "utf-8"));
+if (process.argv.length < 4) {
+    process.stdout.write(`Usage: set-env-vars.js <config_in_json_path> <config_out_json_path>`);
+    process.exit(1);
+}
+const [,, inFile, outFile] = process.argv;
+
+let defaultConfig = JSON.parse(fs.readFileSync(path.resolve(inFile), "utf-8"));
 
 if (process.env.APP_NODE_URL) {
     let pluginConfigs = defaultConfig["plugins"];
@@ -11,4 +17,4 @@ if (process.env.APP_NODE_URL) {
     pluginConfigs[litePluginKey]["nodeUrl"] = process.env.APP_NODE_URL;
 }
 
-fs.writeFileSync(path.join(__dirname, "config.json"), JSON.stringify(defaultConfig, undefined, "\t"));
+fs.writeFileSync(path.resolve(outFile), JSON.stringify(defaultConfig, undefined, "\t"));
